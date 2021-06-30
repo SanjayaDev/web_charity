8a503f71ebdf797ad67100113d24d7e3e39fe7c3
Djohansyah Putra <39056354+emobodigo@users.noreply.github.com>
2021-06-30 20:47:36 +0700

    Add about section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+const filterBtn = document.querySelectorAll(".filter-btn");
+
+filterBtn.forEach((item) => {
+  item.addEventListener("click", () => {
+    filterBtn.forEach((button) => {
+      button.classList.remove("active");
+      item.classList.add("active");
+      let filterValue = item.dataset.filter;
+      $(".grid").isotope({ filter: filterValue });
+    });
+  });
+});
+
 $(".grid").isotope({
   itemSelector: ".grid-item",
   layoutMode: "fitRows",
