a9e244cc620448576ee3acbeb2f1939bca542f41
Djohansyah Putra <39056354+emobodigo@users.noreply.github.com>
2021-07-02 00:12:20 +0700

    Frontend fase awal selesai

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const filterBtn = document.querySelectorAll(".filter-btn");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 filterBtn.forEach((item) => {
   item.addEventListener("click", () => {
@@ -11,8 +14,28 @@ filterBtn.forEach((item) => {
   });
 });
 
+hamburgerMenu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
+
 $(".grid").isotope({
   itemSelector: ".grid-item",
   layoutMode: "fitRows",
   transitionDuration: "0.6s",
 });
+
+const closeMenu = () => {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+};
